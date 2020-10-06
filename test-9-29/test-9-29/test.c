1724d7066d4bb09562006c5dbd101386cede05d3
zhangshunqiang-a <910139647@qq.com>
2020-10-06 21:09:29 +0800

    提交代码

---
+++ test-9-29/test-9-29/test.c
@@ -0,0 +1,92 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+ #include <stdio.h>
+//int main ()
+//{
+//{
+//	int i=0;
+//	int n=0;
+//	int ret=1;
+//	int sum=0;
+//	
+//	for	(n=1;n<=3;n++)
+//	{	ret=1;
+//		for(i=1;i<=n;i++)
+//	{
+//		ret=ret*i;
+//		}
+//
+//		sum=sum+ret; 
+//	}
+//	
+//		printf("sum =%d\n",sum); 
+//
+//
+//	int arr[]={1,2,3,4,5,6,7,8,9,10};
+//	int k=7;
+//	int i=0;
+//	int  sz=sizeof(arr)/sizeof(arr[0]);
+//		int l=0;
+//		int r=sz-1;
+//		while(l<=r)
+//		{
+//			int mid=(l+r)/2;
+//          if(arr[mid]>k)
+//		  {  r=mid-1;
+//		  }	 else if(arr[mid]<k)
+//		  {
+//			  l=mid+1;
+//		  }	
+//		  else
+//		  {
+//
+//			printf("找到了；下标是： %d\n",mid) ;
+//				break;
+//		}
+//	}
+//	if(l>r)
+//		printf("找不到\n")	;
+//
+//
+//
+//	return 0;
+//}
+//	
+int binary_search(int arr[],int k,int sz)
+{
+	int left=0;
+	int right=sz-1;
+	while(left<=right)
+	{
+		int mid=(left+right)/2;
+		if(arr[mid]<k)
+		{
+			left=mid+1;
+		}
+		else if(arr[mid]>k)
+		{
+			right=mid-1;
+		}
+		else
+		{
+			return mid;
+		}
+	}
+	return -1;
+}
+
+int main()
+{
+	int arr[]={1,2,3,4,5,6,7,8,9,10};
+	int k=5;
+	int sz=sizeof(arr)/sizeof(arr[0]);
+	int ret=binary_search(arr,k,sz);
+	if(ret==-1)
+	{
+		printf("找不到指定数字\n");
+	}
+	else
+	{
+		printf("找到了，下标是；%d\n",ret);
+	}
+	return 0;
+}
